3f1387395acfe210fb0ea483ed1698721d9b79fd
migrobin <mrobin@outlook.pt>
2023-12-02 03:24:43 +0000

    day1 part2

--- typescript/day1/day1.js
+++ typescript/day1/day1.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const input = (0, fs_1.readFileSync)("data1", "utf-8");
-const data = input.split("\n");
-console.log(data);
 const part1 = () => {
+    const input = (0, fs_1.readFileSync)("data1", "utf-8");
+    const data = input.split("\n");
+    //console.log(data);
     let solutions = [];
     let first = undefined;
     let last = undefined;
@@ -22,11 +22,61 @@ const part1 = () => {
             first = undefined;
         }
     }
-    console.log(solutions);
+    // console.log(solutions);
     return solutions.reduce((a, b) => a + b, 0).toString();
 };
 const part2 = () => {
-    return "part2";
+    const input = (0, fs_1.readFileSync)("data2", "utf-8");
+    const data = input.split("\n");
+    // console.log(data);
+    let solutions = [];
+    let first;
+    let last;
+    const digits = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    for (const line of data) {
+        first = last = 0;
+        for (let i = 0; i < line.length; i++) {
+            if (Number(line[i])) {
+                first = parseInt(line[i]);
+            }
+            else {
+                digits.forEach((digit, index) => {
+                    if (line.startsWith(digit, i)) {
+                        first = index + 1;
+                    }
+                });
+            }
+            if (first !== 0)
+                break;
+        }
+        for (let i = line.length - 1; i >= 0; i--) {
+            if (Number(line[i])) {
+                last = parseInt(line[i]);
+            }
+            else {
+                digits.forEach((digit, index) => {
+                    if (line.startsWith(digit, i)) {
+                        last = index + 1;
+                    }
+                });
+            }
+            if (last !== 0)
+                break;
+        }
+        solutions.push(parseInt(first.toString() + last.toString()));
+    }
+    // console.log(solutions);
+    return solutions.reduce((a, b) => a + b, 0).toString();
 };
 console.log("Day 1 - Part 1:", part1());
 console.log("Day 1 - Part 2:", part2());
--- typescript/day1/day1.ts
+++ typescript/day1/day1.ts
@@ -1,10 +1,9 @@
 import { readFileSync } from "fs";
 
-const input: string = readFileSync("data1", "utf-8");
-const data = input.split("\n");
-console.log(data);
-
 const part1 = (): string => {
+    const input: string = readFileSync("data1", "utf-8");
+    const data = input.split("\n");
+    //console.log(data);
     let solutions: number[] = [];
     let first: number | undefined = undefined;
     let last: number | undefined = undefined;
@@ -22,12 +21,63 @@ const part1 = (): string => {
             first = undefined;
         }
     }
-    console.log(solutions);
+    // console.log(solutions);
     return solutions.reduce((a, b) => a + b, 0).toString();
 };
 
 const part2 = (): string => {
-    return "part2";
+    const input: string = readFileSync("data2", "utf-8");
+    const data = input.split("\n");
+    // console.log(data);
+    let solutions: number[] = [];
+    let first: number;
+    let last: number;
+    const digits = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    for (const line of data) {
+        first = last = 0;
+
+        for (let i = 0; i < line.length; i++) {
+            if (Number(line[i])) {
+                first = parseInt(line[i]);
+            } else {
+                digits.forEach((digit, index) => {
+                    if (line.startsWith(digit, i)) {
+                        first = index + 1;
+                    }
+                });
+            }
+
+            if (first !== 0) break;
+        }
+
+        for (let i = line.length - 1; i >= 0; i--) {
+            if (Number(line[i])) {
+                last = parseInt(line[i]);
+            } else {
+                digits.forEach((digit, index) => {
+                    if (line.startsWith(digit, i)) {
+                        last = index + 1;
+                    }
+                });
+            }
+            if (last !== 0) break;
+        }
+
+        solutions.push(parseInt(first.toString() + last.toString()));
+    }
+    // console.log(solutions);
+    return solutions.reduce((a, b) => a + b, 0).toString();
 };
 
 console.log("Day 1 - Part 1:", part1());

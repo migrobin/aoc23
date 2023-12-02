90991014ce7484bcdf360df90e421c05e6e8a087
migrobin <mrobin@outlook.pt>
2023-12-02 16:46:21 +0000

    day2 part 2

--- typescript/day2/day2.js
+++ typescript/day2/day2.js
@@ -38,7 +38,37 @@ const part1 = () => {
     return result.reduce((a, b) => a + b, 0).toString();
 };
 const part2 = () => {
-    return "part2";
+    const maxColor = new Map();
+    let power;
+    const games = data.map((line) => line.map((element) => element
+        .split(";")
+        .map((element) => element.split(",").map((element) => element.trim()))));
+    games.forEach((game) => {
+        maxColor.set("red", 0);
+        maxColor.set("green", 0);
+        maxColor.set("blue", 0);
+        power = 1;
+        console.log("game", game[0][0][0].replace("Game ", ""));
+        game[1].forEach((gameSet) => {
+            gameSet.forEach((draw) => {
+                let drawArr = draw.split(" ");
+                maxColor.forEach((value, key) => {
+                    if (key === drawArr[1]) {
+                        if (Number(drawArr[0]) > value) {
+                            maxColor.set(key, Number(drawArr[0]));
+                        }
+                    }
+                });
+            });
+        });
+        console.log("game ", game[0][0][0].replace("Game ", ""), maxColor);
+        maxColor.forEach((value, key) => {
+            power *= value;
+        });
+        console.log("power", power);
+        result.push(power);
+    });
+    return result.reduce((a, b) => a + b, 0).toString();
 };
-console.log("Day 1 - Part 1:", part1());
+//console.log("Day 1 - Part 1:", part1());
 console.log("Day 1 - Part 2:", part2());

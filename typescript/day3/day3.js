fb36ccd310fc4500adf8c0e3006b94f323eadd41
migrobin <mrobin@outlook.pt>
2023-12-03 23:43:35 +0000

    transf

--- typescript/day3/day3.js
+++ typescript/day3/day3.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
-const input = (0, fs_1.readFileSync)("data1", "utf-8").split("\n");
+const input = (0, fs_1.readFileSync)("data", "utf-8").split("\n");
 function isNumber(x) {
     return !isNaN(x);
 }
@@ -101,7 +101,88 @@ const part1 = () => {
     return result.reduce((a, b) => a + b).toString();
 };
 const part2 = () => {
-    return "part2";
+    const data = input.map((line) => line.split(""));
+    const toCheck = (row, col) => {
+        let arrToCheck = [];
+        if (row > 0) {
+            arrToCheck.push([row - 1, col]);
+            if (col > 0) {
+                arrToCheck.push([row - 1, col - 1]);
+                arrToCheck.push([row, col - 1]);
+            }
+            if (col < data[0].length - 1) {
+                arrToCheck.push([row - 1, col + 1]);
+                arrToCheck.push([row, col + 1]);
+            }
+        }
+        if (row < data[0].length - 1) {
+            arrToCheck.push([row + 1, col]);
+            if (col > 0) {
+                arrToCheck.push([row, col - 1]);
+                arrToCheck.push([row + 1, col - 1]);
+            }
+            if (col < data[0].length - 1) {
+                arrToCheck.push([row, col + 1]);
+                arrToCheck.push([row + 1, col + 1]);
+            }
+        }
+        console.log("row", row, "col", col);
+        // console.table(arrToCheck);
+        return arrToCheck;
+    };
+    function has2Hits(row, col) {
+        let num1 = 1;
+        let num2 = 1;
+        let hits = 0;
+        const hitsArray = [];
+        toCheck(row, col).forEach((position) => {
+            if (isNumber(+data[position[0]][position[1]]))
+                hitsArray.push(arrMap.get([row, col]));
+        });
+        console.log(hitsArray);
+        return num1 * num2;
+    }
+    console.table(data);
+    const result = [];
+    let temp = "";
+    const arrMap = new Map();
+    let start = -1;
+    const stars = [];
+    let id = 0;
+    for (let row = 0; row < data.length; row++) {
+        for (let col = 0; col < data[0].length; col++) {
+            if (data[row][col] === "*")
+                stars.push([row, col]);
+            if (isNumber(+data[row][col])) {
+                if (start === -1)
+                    start = col;
+                temp += data[row][col];
+            }
+            else {
+                if (temp !== "") {
+                    for (let i = start; i < col; i++) {
+                        arrMap.set([row, col], {
+                            id: id,
+                            row: row,
+                            start: start,
+                            end: col - 1,
+                        });
+                    }
+                    result.push(Number(temp));
+                    id++;
+                    start = -1;
+                }
+                temp = "";
+            }
+        }
+    }
+    console.log(arrMap);
+    console.log("stars", stars);
+    console.log("result", result);
+    for (let star of stars) {
+        console.log(has2Hits(star[0], star[1]));
+    }
+    return result.reduce((a, b) => a + b).toString();
 };
-console.log("Day 1 - Part 1:", part1());
+//console.log("Day 1 - Part 1:", part1());
 console.log("Day 1 - Part 2:", part2());
--- typescript/day3/day3.ts
+++ typescript/day3/day3.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
-const input: string[] = readFileSync("data1", "utf-8").split("\n");
+const input: string[] = readFileSync("data", "utf-8").split("\n");
 
 function isNumber(x: number): boolean {
     return !isNaN(x);
@@ -113,8 +113,99 @@ const part1 = (): string => {
 };
 
 const part2 = (): string => {
-    return "part2";
+    const data = input.map((line) => line.split(""));
+
+    const toCheck = (row: number, col: number) => {
+        let arrToCheck = [];
+
+        if (row > 0) {
+            arrToCheck.push([row - 1, col]);
+
+            if (col > 0) {
+                arrToCheck.push([row - 1, col - 1]);
+                arrToCheck.push([row, col - 1]);
+            }
+
+            if (col < data[0].length - 1) {
+                arrToCheck.push([row - 1, col + 1]);
+                arrToCheck.push([row, col + 1]);
+            }
+        }
+        if (row < data[0].length - 1) {
+            arrToCheck.push([row + 1, col]);
+
+            if (col > 0) {
+                arrToCheck.push([row, col - 1]);
+                arrToCheck.push([row + 1, col - 1]);
+            }
+            if (col < data[0].length - 1) {
+                arrToCheck.push([row, col + 1]);
+                arrToCheck.push([row + 1, col + 1]);
+            }
+        }
+
+        console.log("row", row, "col", col);
+        // console.table(arrToCheck);
+        return arrToCheck;
+    };
+
+    function has2Hits(row: number, col: number): number {
+        let num1 = 1;
+        let num2 = 1;
+        let hits = 0;
+        const hitsArray: number[] = [];
+
+        toCheck(row, col).forEach((position) => {
+            if (isNumber(+data[position[0]][position[1]]))
+                hitsArray.push(arrMap.get([row, col]));
+        });
+        console.log(hitsArray);
+        return num1 * num2;
+    }
+
+    console.table(data);
+
+    const result: number[] = [];
+    let temp = "";
+    const arrMap = new Map();
+    let start = -1;
+
+    const stars = [];
+    let id = 0;
+    for (let row = 0; row < data.length; row++) {
+        for (let col = 0; col < data[0].length; col++) {
+            if (data[row][col] === "*") stars.push([row, col]);
+
+            if (isNumber(+data[row][col])) {
+                if (start === -1) start = col;
+                temp += data[row][col];
+            } else {
+                if (temp !== "") {
+                    for (let i = start; i < col; i++) {
+                        arrMap.set([row, col], {
+                            id: id,
+                            row: row,
+                            start: start,
+                            end: col - 1,
+                        });
+                    }
+                    result.push(Number(temp));
+                    id++;
+                    start = -1;
+                }
+                temp = "";
+            }
+        }
+    }
+    console.log(arrMap);
+    console.log("stars", stars);
+    console.log("result", result);
+
+    for (let star of stars) {
+        console.log(has2Hits(star[0], star[1]));
+    }
+    return result.reduce((a, b) => a + b).toString();
 };
 
-console.log("Day 1 - Part 1:", part1());
+//console.log("Day 1 - Part 1:", part1());
 console.log("Day 1 - Part 2:", part2());

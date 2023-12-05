62c539d2e056bafd0b10a01f11fca15bce41b75a
migrobin <mrobin@outlook.pt>
2023-12-05 19:45:59 +0000

    gitignore

--- typescript/day5/day5.js
+++ typescript/day5/day5.js
@@ -8,17 +8,14 @@ const part1 = () => {
         .map((char) => parseInt(char));
     const data = input
         .filter((_, index) => index > 0)
-        .map((el) => el
+        .map((x) => x
         .split("\n")
         .filter((char) => !isNaN(parseInt(char[0])))
         .map((el) => el.split(" ").map((el) => parseInt(el))));
-    // console.log(data);
     function mapper(seed, turnIndex) {
-        //console.log("seed", seed);
         if (turnIndex === data.length)
             return seed;
         for (let i = 0; i < data[turnIndex].length; i++) {
-            //  console.log("line", data[turnIndex][i]);
             if (seed < data[turnIndex][i][1])
                 continue;
             if (seed >= data[turnIndex][i][1] && seed <= data[turnIndex][i][1] + data[turnIndex][i][2])
@@ -26,11 +23,77 @@ const part1 = () => {
         }
         return mapper(seed, turnIndex + 1);
     }
-    const result = seeds.map((seed) => mapper(seed, 0)).sort((a, b) => a - b);
-    return result[0].toString();
+    console.log("mapper", mapper(82, 0));
+    return seeds
+        .map((seed) => mapper(seed, 0))
+        .sort((a, b) => a - b)[0]
+        .toString();
 };
 const part2 = () => {
-    return "part2";
+    const seeds = input[0]
+        .split(":")[1]
+        .split(" ")
+        .filter((el) => el !== "")
+        .map((char) => parseInt(char));
+    const data = input
+        .filter((_, index) => index > 0)
+        .map((x) => x
+        .split("\n")
+        .filter((char) => !isNaN(parseInt(char[0])))
+        .map((el) => el.split(" ").map((el) => parseInt(el))));
+    const seenSeed = new Map();
+    let count = 0;
+    let rec = 0;
+    function mapper(seed, turnIndex) {
+        if (turnIndex === data.length)
+            return seed;
+        for (let i = 0; i < data[turnIndex].length; i++) {
+            if (seed < data[turnIndex][i][1])
+                continue;
+            if (seenSeed.has(JSON.stringify([data[turnIndex][i][1] + i, turnIndex]))) {
+                return seenSeed.get(JSON.stringify([data[turnIndex][i][1] + i, turnIndex]));
+            }
+            if (seed >= data[turnIndex][i][1] && seed <= data[turnIndex][i][1] + data[turnIndex][i][2]) {
+                return mapper(data[turnIndex][i][0] - data[turnIndex][i][1] + seed, turnIndex + 1);
+            }
+        }
+        if (seenSeed.has(JSON.stringify([seed, turnIndex + 1]))) {
+            return seenSeed.get(JSON.stringify([seed, turnIndex + 1]));
+        }
+        else {
+            return mapper(seed, turnIndex + 1);
+        }
+    }
+    let min = Infinity;
+    let temp;
+    // console.log(seeds); [ 79, 14, 55, 13 ]
+    //seeds
+    seeds.forEach((seed, index) => {
+        if (index % 2 === 0) {
+            for (let i = 0; i < seeds[index + 1]; i++) {
+                if (seenSeed.has(JSON.stringify([seed + i, 0]))) {
+                    temp = seenSeed.get(JSON.stringify([seed + i, 0]));
+                    count++;
+                    console.log("hit outside ", count, JSON.stringify([seed + i, 0]));
+                }
+                else {
+                    temp = mapper(seed + i, 0);
+                    seenSeed.set(JSON.stringify([seed + i - 1, 0]), temp);
+                    // console.log("setting outside ", JSON.stringify([seed + i, 0]), "=>", temp);
+                }
+                // temp = mapper(seed + i, 0);
+                // console.log(seed + i, "=>", temp);
+                if (temp < min)
+                    min = temp;
+            }
+        }
+    });
+    //  console.log("mapper 82", mapper(82, 0));
+    // console.log(seenSeed);
+    return min.toString();
 };
-console.log("Day 1 - Part 1:", part1());
+//console.log("Day 1 - Part 1:", part1());
+//old score 57451710
+console.time("time");
 console.log("Day 1 - Part 2:", part2());
+console.timeEnd("time");
--- typescript/day5/day5.ts
+++ typescript/day5/day5.ts
@@ -1,3 +1,4 @@
+import { log } from "console";
 import { readFileSync } from "fs";
 
 const input: string[] = readFileSync("data1", "utf-8").split("\n\n");
@@ -11,8 +12,8 @@ const part1 = (): string => {
 
     const data = input
         .filter((_, index) => index > 0)
-        .map((el) =>
-            el
+        .map((x) =>
+            x
                 .split("\n")
                 .filter((char) => !isNaN(parseInt(char[0])))
                 .map((el) => el.split(" ").map((el) => parseInt(el)))
@@ -29,14 +30,88 @@ const part1 = (): string => {
         return mapper(seed, turnIndex + 1);
     }
 
-    const result = seeds.map((seed) => mapper(seed, 0)).sort((a, b) => a - b);
-
-    return result[0].toString();
+    console.log("mapper", mapper(82, 0));
+    return seeds
+        .map((seed) => mapper(seed, 0))
+        .sort((a, b) => a - b)[0]
+        .toString();
 };
 
 const part2 = (): string => {
-    return "part2";
+    const seeds = input[0]
+        .split(":")[1]
+        .split(" ")
+        .filter((el) => el !== "")
+        .map((char) => parseInt(char));
+
+    const data = input
+        .filter((_, index) => index > 0)
+        .map((x) =>
+            x
+                .split("\n")
+                .filter((char) => !isNaN(parseInt(char[0])))
+                .map((el) => el.split(" ").map((el) => parseInt(el)))
+        );
+
+    const seenSeed = new Map();
+    let count = 0;
+    let rec = 0;
+
+    function mapper(seed: number, turnIndex: number): number {
+        if (turnIndex === data.length) return seed;
+
+        for (let i = 0; i < data[turnIndex].length; i++) {
+            if (seed < data[turnIndex][i][1]) continue;
+
+            if (seenSeed.has(JSON.stringify([data[turnIndex][i][1] + i, turnIndex]))) {
+                return seenSeed.get(JSON.stringify([data[turnIndex][i][1] + i, turnIndex]));
+            }
+
+            if (seed >= data[turnIndex][i][1] && seed <= data[turnIndex][i][1] + data[turnIndex][i][2]) {
+                return mapper(data[turnIndex][i][0] - data[turnIndex][i][1] + seed, turnIndex + 1);
+            }
+        }
+
+        if (seenSeed.has(JSON.stringify([seed, turnIndex + 1]))) {
+            return seenSeed.get(JSON.stringify([seed, turnIndex + 1]));
+        } else {
+            return mapper(seed, turnIndex + 1);
+        }
+    }
+
+    let min = Infinity;
+    let temp;
+    // console.log(seeds); [ 79, 14, 55, 13 ]
+    //seeds
+    seeds.forEach((seed, index) => {
+        if (index % 2 === 0) {
+            for (let i = 0; i < seeds[index + 1]; i++) {
+                if (seenSeed.has(JSON.stringify([seed + i, 0]))) {
+                    temp = seenSeed.get(JSON.stringify([seed + i, 0]));
+                    count++;
+                    console.log("hit outside ", count, JSON.stringify([seed + i, 0]));
+                } else {
+                    temp = mapper(seed + i, 0);
+                    seenSeed.set(JSON.stringify([seed + i - 1, 0]), temp);
+                    // console.log("setting outside ", JSON.stringify([seed + i, 0]), "=>", temp);
+                }
+                // temp = mapper(seed + i, 0);
+                // console.log(seed + i, "=>", temp);
+                if (temp < min) min = temp;
+            }
+        }
+    });
+
+    //  console.log("mapper 82", mapper(82, 0));
+
+    // console.log(seenSeed);
+    return min.toString();
 };
 
-console.log("Day 1 - Part 1:", part1());
+//console.log("Day 1 - Part 1:", part1());
+
+//old score 57451710
+
+console.time("time");
 console.log("Day 1 - Part 2:", part2());
+console.timeEnd("time");

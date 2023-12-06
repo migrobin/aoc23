1ef6b4b93dd0fbeb9e85491be5e9f2a3622291fa
migrobin <mrobin@outlook.pt>
2023-12-06 01:14:09 +0000

    brute

--- typescript/day5/day5.js
+++ typescript/day5/day5.js
@@ -41,8 +41,7 @@ const part2 = () => {
         .split("\n")
         .filter((char) => !isNaN(parseInt(char[0])))
         .map((el) => el.split(" ").map((el) => parseInt(el))));
-    const seenSeed = new Map();
-    let count = 0;
+    let seenSeed = {};
     let rec = 0;
     function mapper(seed, turnIndex) {
         if (turnIndex === data.length)
@@ -50,39 +49,23 @@ const part2 = () => {
         for (let i = 0; i < data[turnIndex].length; i++) {
             if (seed < data[turnIndex][i][1])
                 continue;
-            if (seenSeed.has(JSON.stringify([data[turnIndex][i][1] + i, turnIndex]))) {
-                return seenSeed.get(JSON.stringify([data[turnIndex][i][1] + i, turnIndex]));
-            }
             if (seed >= data[turnIndex][i][1] && seed <= data[turnIndex][i][1] + data[turnIndex][i][2]) {
                 return mapper(data[turnIndex][i][0] - data[turnIndex][i][1] + seed, turnIndex + 1);
             }
         }
-        if (seenSeed.has(JSON.stringify([seed, turnIndex + 1]))) {
-            return seenSeed.get(JSON.stringify([seed, turnIndex + 1]));
-        }
-        else {
-            return mapper(seed, turnIndex + 1);
-        }
+        return mapper(seed, turnIndex + 1);
     }
     let min = Infinity;
-    let temp;
+    let temp = 0;
+    let count = 0;
     // console.log(seeds); [ 79, 14, 55, 13 ]
     //seeds
     seeds.forEach((seed, index) => {
         if (index % 2 === 0) {
             for (let i = 0; i < seeds[index + 1]; i++) {
-                if (seenSeed.has(JSON.stringify([seed + i, 0]))) {
-                    temp = seenSeed.get(JSON.stringify([seed + i, 0]));
-                    count++;
-                    console.log("hit outside ", count, JSON.stringify([seed + i, 0]));
-                }
-                else {
-                    temp = mapper(seed + i, 0);
-                    seenSeed.set(JSON.stringify([seed + i - 1, 0]), temp);
-                    // console.log("setting outside ", JSON.stringify([seed + i, 0]), "=>", temp);
-                }
-                // temp = mapper(seed + i, 0);
-                // console.log(seed + i, "=>", temp);
+                count++;
+                // seenSeed[JSON.stringify([seed + i - 1, 0])] = temp;
+                temp = mapper(seed + i - 1, 0);
                 if (temp < min)
                     min = temp;
             }
@@ -90,6 +73,7 @@ const part2 = () => {
     });
     //  console.log("mapper 82", mapper(82, 0));
     // console.log(seenSeed);
+    console.log("seeds", count);
     return min.toString();
 };
 //console.log("Day 1 - Part 1:", part1());

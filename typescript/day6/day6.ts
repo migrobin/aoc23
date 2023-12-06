2fbb93cf63faecc3b2abba21455d358b3070a483
migrobin <mrobin@outlook.pt>
2023-12-06 11:48:50 +0000

    parsing

--- typescript/day6/day6.ts
+++ typescript/day6/day6.ts
@@ -3,11 +3,43 @@ import { readFileSync } from "fs";
 //Time:      7  15   30
 //Distance:  9  40  200
 
-const input: string[] = readFileSync("data", "utf-8").split("\n");
+function transpose(matrix: number[][]): number[][] {
+    const transposed: number[][] = [[]];
+
+    for (let row = 0; row < matrix.length; row++) {
+        transposed.push([]);
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++) {
+            transposed[col].push(matrix[row][col]);
+        }
+    }
+    return transposed;
+}
+
+const input: number[][] = readFileSync("data", "utf-8")
+    .split("\n")
+    .map((line) =>
+        line
+            .split(":")
+            .filter((_, index) => index % 2 === 1)
+            .map((chars) =>
+                chars
+                    .trim()
+                    .split(" ")
+                    .filter((el) => el !== "")
+                    .map((char) => parseInt(char))
+            )
+    )
+    .flat();
+
+console.log(input);
+console.log(transpose(input));
 
 const part1 = (): string => {
     for (let line of input) {
-        console.log(line);
+        //        console.log(line);
     }
     return "part1";
 };
